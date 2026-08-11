20b56bfcb17f2d1b6e53b3f4dc8284bd616db9fa
BobTheMan7891 <febosutherland2.0@gmail.com>
2026-08-11 20:08:58 +0400

    Download button for the CV

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active')
 })
+
+
+const cvDownloadButton = document.querySelector('#cv-download')
+
+cvDownloadButton.addEventListener('click', () =>{
+    cvDownloadButton.classList.add('active');
+})
